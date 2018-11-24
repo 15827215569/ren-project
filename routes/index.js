242fac0c1097e73bf89b2c8e2252dac6c64ab277
15827215569 <136804466@qq.com>
2018-11-24 14:51:32 +0800

    抄了一个静态页面

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/phone.html', function (req, res) {
+  res.render('phone');
+});
+router.get('/brand.html', function (req, res) {
+  res.render('brand');
+});
+router.get('/user.html', function (req, res) {
+  res.render('user');
+});
+router.get('/info.html', function (req, res) {
+  res.render('info');
 });
 
 module.exports = router;
